e410aff01c521355d5b0d019c59fc4e729551c05
londonderry-air <hello.fugetsu@gmail.com>
2022-04-27 14:53:45 +0900

    fix nextauth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,7 +16,7 @@ export default NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? ''
     })
   ],
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET ?? 'secret',
+  secret: process.env.NEXT_PUBLIC_SECRET ?? 'secret',
   callbacks: {
     async signIn({ user }) {
       const { body: userCount } = await apiClient.v1.admin.user.count.get()
