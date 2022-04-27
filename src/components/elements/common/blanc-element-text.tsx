b286c0ae1c185183266930dfcba89131629083e4
londonderry-air <hello.fugetsu@gmail.com>
2022-04-27 18:29:25 +0900

    fix text element

--- src/components/elements/common/blanc-element-text.tsx
+++ src/components/elements/common/blanc-element-text.tsx
@@ -1,4 +1,6 @@
 import { BlancElement } from '$/types/_element'
+import { AlignBox } from '~/components/atoms/box/align'
+import { moduler } from '~/utils/styles'
 import {
   BlancElementText,
   BlancTextElementProps
@@ -6,14 +8,17 @@ import {
 
 export const BlancCommonText = (props: BlancCommonTextProps) => {
   return (
-    <BlancElementText
-      data={props.data}
-      font={{ size: '2em', weight: '600' }}
-      padding={'30px'}
-      markup={'p'}
-      align={'center'}
-      placeholder={'テキストを入力してください'}
-    />
+    <AlignBox align={'center'} width={'60ch'} margin={'0 0 30px 0'}>
+      <BlancElementText
+        data={props.data}
+        font={{ size: moduler(-1), weight: '400' }}
+        markup={'p'}
+        align={'left'}
+        h_space={'0.06em'}
+        v_space={'1.8em'}
+        placeholder={'テキストを入力してください'}
+      />
+    </AlignBox>
   )
 }
 
