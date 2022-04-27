38a300a01d51ddffeb4daf621b2c7823d681ab63
londonderry-air <hello.fugetsu@gmail.com>
2022-04-27 17:22:48 +0900

    fix storage function

--- server/api/v1/storage/controller.ts
+++ server/api/v1/storage/controller.ts
@@ -18,7 +18,7 @@ export default defineController(() => ({
         projectId: process.env.GCP_PROJECT_ID,
         credentials: {
           client_email: process.env.GCP_CLIENT_EMAIL,
-          private_key: process.env.GCP_PRIVATE_KEY
+          private_key: process.env.GCP_PRIVATE_KEY ? process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n') : ''
         }
       })
       const bucket = storage.bucket(process.env.GCP_BUCKETNAME)
